70cb1e0daf365799c968d257ee87126223070aa4
Przemysław Białek <przemek.bialek@gmail.com>
2024-11-20 16:20:28 +0100

    doc: add plotter docs

--- src/lib/Plotter.js
+++ src/lib/Plotter.js
@@ -1,6 +1,7 @@
 "use strict";
 import { Color } from "./Color.js";
 import { Complex } from "./Complex.js";
+import { Position } from "./Position.js";
 
 export class Plotter {
   #plotBounds = { left: null, right: null, top: null, bottom: null };
@@ -14,10 +15,16 @@ export class Plotter {
   /** @type {CanvasRenderingContext2D} */
   context;
 
+  /**
+   * plot width in pixels
+   */
   get width() {
     return this.context.canvas.offsetWidth;
   }
 
+  /**
+   * plot height in pixels
+   */
   get height() {
     return this.context.canvas.offsetHeight;
   }
@@ -39,16 +46,21 @@ export class Plotter {
     );
   }
 
-  setBounds({ left, right, top, bottom }) {
-    this.#plotBounds.left = left;
-    this.#plotBounds.right = right;
-    this.#plotBounds.top = top;
-    this.#plotBounds.bottom = bottom;
-
-    this.#xScale = (right - left) / this.width;
-    this.#yScale = (top - bottom) / this.height;
+  /**
+   * Sets new plot bounds
+   * @param {{ left: number, right: number, top: number, bottom:number }} bounds new plot bounds
+   */
+  setBounds(bounds) {
+    this.#plotBounds = bounds;
+    this.#xScale = (bounds.right - bounds.left) / this.width;
+    this.#yScale = (bounds.top - bounds.bottom) / this.height;
   }
 
+  /**
+   * Maps position in pixels to position in plot coordinates
+   * @param {Position} param0 position in pixels
+   * @returns complex number in plot coordinates
+   */
   mapToPlotCoordinates({ x, y }) {
     return new Complex(
       x * this.#xScale + this.#plotBounds.left,
@@ -56,28 +68,39 @@ export class Plotter {
     );
   }
 
-  setPoint({ x, y }, color) {
+  /**
+   * Set pixel color at provided position
+   * @param {Position} param0 position in pixels
+   * @param {*} color color to set
+   */
+  #setPoint({ x, y }, color) {
     const bytesPerPixel = 4;
-    const { plotImage: plotImg } = this;
+    const { plotImage } = this;
 
-    const pixelIdx = (y * plotImg.width + x) * bytesPerPixel;
+    const pixelIdx = (y * plotImage.width + x) * bytesPerPixel;
 
-    plotImg.data[pixelIdx] = color.r;
-    plotImg.data[pixelIdx + 1] = color.g;
-    plotImg.data[pixelIdx + 2] = color.b;
-    plotImg.data[pixelIdx + 3] = color.a;
+    plotImage.data[pixelIdx] = color.r;
+    plotImage.data[pixelIdx + 1] = color.g;
+    plotImage.data[pixelIdx + 2] = color.b;
+    plotImage.data[pixelIdx + 3] = color.a;
   }
 
   /**
-   *
-   * @param {*} generate
+   * This callback is displayed as part of the Requester class.
+   * @callback PlotCallback
+   * @param {Complex} pos processed number
+   * @returns color index number between 0-255
+   */
+  /**
+   * Prepares plot using provide function
+   * @param {PlotCallback} generate
    */
   plot(generate) {
     for (let x = 0; x < this.width; x++) {
       for (let y = 0; y < this.height; y++) {
         const point = this.mapToPlotCoordinates({ x, y });
         const colorIdx = generate(point);
-        this.setPoint({ x, y }, this.#colorMap[colorIdx]);
+        this.#setPoint({ x, y }, this.#colorMap[colorIdx]);
       }
     }
 
